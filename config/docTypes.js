549ac92f4c19fde293f928f86b03582b45518a66
Afla K K <aflaethicfin@gmail.com>
2026-08-18 13:42:12 +0530

    deliverynote

--- config/docTypes.js
+++ config/docTypes.js
@@ -10,7 +10,7 @@ const DOC_TYPES = {
   7: { folder: 'Creditnote',    label: 'Credit Note' },
   8: { folder: 'Purchasereturn',    label: 'Purchase Return' },
   9: { folder: 'Salesereturn',    label: 'Sales Return' },
-  10: { folder: 'Deliverynote',    label: 'Delivery Note' },
+  10: { folder: 'deliverynote',    label: 'Delivery Note' },
 };
 
 const DEFAULT_DOC_TYPE = 1;
